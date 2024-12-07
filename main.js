943a88198c9d9bac43d5170831d53dfd55598035
yashChouriya <yashchouriya131+github@gmail.com>
2024-12-07 23:15:58 +0530

    added new proj

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
+
+    // Load the index.html file
+    mainWindow.loadFile('index.html');
+
+    // Open the DevTools in development mode
+    // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
---
+++ renderer.js
@@ -0,0 +1,4 @@
+// This file will handle all the renderer process logic
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Renderer process started');
+});
